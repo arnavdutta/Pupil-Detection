2dabd302340d5aedba12d175ecd3540ca8c55313
arnavdutta <39775516+arnavdutta@users.noreply.github.com>
2019-04-29 14:13:57 +0530

    Added pupil detection from a video file

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 3. os
 4. shutil
 
-### Steps involved
+### Steps involved for detecting pupil from images
 
 1. Loop through all the images from the input folder
 2. Convert the images from BGR colorspace to grayscale
@@ -19,3 +19,17 @@
 7. Get the `x`,`y`,`w`,`h` from the `cv2.boundingRect`.
 8. Draw a circle using `cv2.circle`
 9. Save the results in the output folder
+
+### Steps involved for detecting pupil from a video of an eye
+
+1. Capture the video file using `cv2.VideoCapture` 
+2. Loop the video continuously and save the frames of the video using `read()`
+3. Get the exact area of the eye (ROI) from the whole frame.
+4. Convert the ROI region into Grayscale then use GaussianBlur as well as medianBlur to reduce the noise.
+5. Threshold the ROI region to get the contours and select only the largest contour as it will be pupil.
+6. Once, contour is detected using `cv2.findContours`, get the `x`,`y`,`w`,`h` from the `cv2.boundingRect`
+7. Draw a circle using `cv2.circle`
+
+`NOTE`: use `cap.set(cv2.CAP_PROP_POS_FRAMES, 0)` to loop back the video from beginning otherwise, it will throw error as there is no frame to capture.
+
+`NOTE`: At the end `release()` the video file.
---
+++ pupil_detect_video/pupil_detect_video.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('video.mp4')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        roi = frame[50:500, 300:800]
+        rows, cols, _ = roi.shape
+        gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        gray_roi = cv2.GaussianBlur(gray_roi, (11, 11), 0)
+        gray_roi = cv2.medianBlur(gray_roi, 3)
+
+        threshold = cv2.threshold(gray_roi, 15, 255, cv2.THRESH_BINARY_INV)[1]
+        contours = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+        contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+
+        for cnt in contours:
+            (x, y, w, h) = cv2.boundingRect(cnt)
+            cv2.circle(roi, (x + int(w/2), y + int(h/2)), int((h)/3), (0, 0, 255), 2)
+            cv2.line(roi, (x + int(w/2), 0), (x + int(w/2), rows), (50, 200, 0), 1)
+            cv2.line(roi, (0, y + int(h/2)), (cols , y + int(h/2)), (50, 200, 0), 1)
+            cv2.putText(roi, text = '[Press Q to Exit]', org = (int(cols - 180), int(rows - 15)), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.6, color = (0, 0, 0))
+            cv2.putText(threshold, text = '[Press Q to Exit]', org = (int(cols - 180), int(rows - 15)), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.6, color = (255, 255, 255))
+            cv2.putText(gray_roi, text = '[Press Q to Exit]', org = (int(cols - 180), int(rows - 15)), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.6, color = (0, 0, 0))
+            break
+            
+        cv2.imshow("roi", roi)
+        # cv2.imshow('Threshold', threshold)
+        # cv2.imshow('gray_roi', gray_roi)
+        
+    else:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        
+    if cv2.waitKey(15) & 0xFF == ord('q'): # Press 'Q' on the keyboard to exit the playback
+        break
+cap.release()
+cv2.destroyAllWindows()
